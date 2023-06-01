cdd415f5e804efa39e10cf58fc15f25d49773edd
ChipCE <login721@gmail.com>
2023-06-01 13:11:52 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# klipper-dynamic-scv-slicer-post-processing
+# Klipper Dynamic Square Corner Velocity
+
+Inspired by [RomRider](https://github.com/RomRider)'s [klipper-FastGyroidInfill](https://github.com/RomRider/klipper-FastGyroidInfill/blob/main/superslicer/FastGyroidInfill.py). 
+
+- This slicer post-processing script allow you set custom Square Corner Velocity(CSV) not just of infill but also other print moves(outer perimeter/wall, inner perimeter/wall, infill, overhang, top/bottom fill, support).
+- Support Cura, Prusa slicer, super slicer.
+- Does not require any custom macro config.
+
+## Installation (for PrusaSlicer and SuperSlicer)
+
+- Copy <code>dynamicScv.py</code> and <code>dynamicScv.cfg</code> into a folder on your computer. Make sure there is no space in the file path.
+- Open your slicer > <code>Print setting</code> > <code>Output options</code> > <code>Post-processing scripts</code> ans enter the full file path of the <code>dynamicScv.py</code> script.
+- Edit <code>dynamicScv.cfg</code> and enter the suitable SCV for your machine.
+- ※Linux user might need to edit the <code>#!/usr/bin/python3</code> header of <code>dynamicScv.py</code> file to match their system.
+![prusa-slicer-config](./prusa-slicer.png)
+
+## Installation (for Cura)
+- Copy <code>dynamicScv.py</code> into your cura <code>scripts</code> folder. (The location of the folder can be checked in Cura menu <code>Help</code> > <code>Show configuration folder</code>. You might need to create <code>scripts</code> if it does not exist.)
+- Restart Cura 
+- In cura menu <code>Extensions</code> > <code>Post processing</code> -> <code>Modify G-Code</code> and select <code>Dynamic Square Corner Velocity</code>
+- Enter the suitable SCV value for your machine in the right config section.
+
+![cura-config](./cura.png)
---
+++ cura-slicer/dynamicScv.py
@@ -0,0 +1,102 @@
+import re
+from ..Script import Script
+
+
+class DynamicScv(Script):
+
+    def getSettingDataString(self):
+        return """{
+            "name": "Dynamic Square Corner Velocity",
+            "key": "DynamicScv",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "enable_dynamic_scv":
+                {
+                    "label": "Enable",
+                    "description": "Enable dynamic Square Corner Velocity",
+                    "type": "bool",
+                    "default_value": true
+                },
+                "default_scv":
+                {
+                    "label": "    Default SCV",
+                    "description": "Default Square Corner Velocity",
+                    "type": "int",
+                    "default_value": 5,
+                    "minimum_value": "1",
+                    "maximum_value" : "100",
+                    "enabled": "enable_dynamic_scv"
+                },
+                "outer_wall_scv":
+                {
+                    "label": "    Outer wall SCV",
+                    "description": "Outer wall Square Corner Velocity",
+                    "type": "int",
+                    "default_value": 5,
+                    "minimum_value": "1",
+                    "maximum_value" : "100",
+                    "enabled": "enable_dynamic_scv"
+                },
+                "inner_wall_scv":
+                {
+                    "label": "    Inner wall SCV",
+                    "description": "Inner wall Square Corner Velocity",
+                    "type": "int",
+                    "default_value": 10,
+                    "minimum_value": "1",
+                    "maximum_value" : "100",
+                    "enabled": "enable_dynamic_scv"
+                },
+                "skin_scv":
+                {
+                    "label": "    Skin SCV",
+                    "description": "Skin Square Corner Velocity",
+                    "type": "int",
+                    "default_value": 15,
+                    "minimum_value": "1",
+                    "maximum_value" : "100",
+                    "enabled": "enable_dynamic_scv"
+                },
+                "infill_scv":
+                {
+                    "label": "    Infill SCV",
+                    "description": "Infill Square Corner Velocity",
+                    "type": "int",
+                    "default_value": 20,
+                    "minimum_value": "1",
+                    "maximum_value" : "100",
+                    "enabled": "enable_dynamic_scv"
+                }
+            }
+        }"""
+
+    def execute(self, data):
+        enable_dynamic_scv = self.getSettingValueByKey("enable_dynamic_scv")
+        default_scv = self.getSettingValueByKey("default_scv")
+        outer_wall_scv = self.getSettingValueByKey("outer_wall_scv")
+        inner_wall_scv = self.getSettingValueByKey("inner_wall_scv")
+        skin_scv = self.getSettingValueByKey("skin_scv")
+        infill_scv = self.getSettingValueByKey("infill_scv")
+
+        for layerNumber, layerData in enumerate(data):            
+            if enable_dynamic_scv:
+                lines = data[layerNumber].split("\n")
+                for lineNumber,lineData in enumerate(lines):
+                    if lineData.startswith(";TYPE:FILL"):
+                        lines[lineNumber] = lineData + "\n" + "SET_VELOCITY_LIMIT SQUARE_CORNER_VELOCITY=" + str(infill_scv)
+                    elif lineData.startswith(";TYPE:WALL-INNER"):
+                        lines[lineNumber] = lineData + "\n" + "SET_VELOCITY_LIMIT SQUARE_CORNER_VELOCITY=" + str(inner_wall_scv)
+                    elif lineData.startswith(";TYPE:WALL-OUTER"):
+                        lines[lineNumber] = lineData + "\n" + "SET_VELOCITY_LIMIT SQUARE_CORNER_VELOCITY=" + str(outer_wall_scv)
+                    elif lineData.startswith(";TYPE:SKIN"):
+                        lines[lineNumber] = lineData + "\n" + "SET_VELOCITY_LIMIT SQUARE_CORNER_VELOCITY=" + str(skin_scv)
+                    elif lineData.startswith(";TYPE:") or lineData.startswith(";LAYER:"):
+                        lines[lineNumber] = lineData + "\n" + "SET_VELOCITY_LIMIT SQUARE_CORNER_VELOCITY=" + str(default_scv)
+                    else:
+                        continue
+                    
+                data[layerNumber] = "\n".join(lines)
+
+        return data
